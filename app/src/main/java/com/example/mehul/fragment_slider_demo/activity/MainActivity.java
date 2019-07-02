272302e1be080632560b577a6b1e59b1d446a781
sudhir <sudhir@finesofttechnologies.com>
2019-07-02 18:48:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mehul/fragment_slider_demo/activity/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.mehul.fragment_slider_demo.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.mehul.fragment_slider_demo.R;
+import com.example.mehul.fragment_slider_demo.fragments.FragmentOne;
+import com.example.mehul.fragment_slider_demo.fragments.FragmentThree;
+import com.example.mehul.fragment_slider_demo.fragments.FragmentTwo;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static MainActivity instance;
+
+    private FragmentOne fragmentOne;
+    private FragmentTwo fragmentTwo;
+    private FragmentThree fragmentThree;
+    private TabLayout allTabs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        instance=this;
+
+
+        getAllWidgets();
+        bindWidgetsWithAnEvent();
+        setupTabLayout();
+    }
+
+    public static MainActivity getInstance() {
+        return instance;
+    }
+
+    private void getAllWidgets() {
+        allTabs = (TabLayout) findViewById(R.id.tabs);
+    }
+
+    private void setupTabLayout() {
+        fragmentOne = new FragmentOne();
+        fragmentTwo = new FragmentTwo();
+        fragmentThree = new FragmentThree();
+
+        allTabs.addTab(allTabs.newTab().setText("ONE"),true);
+        allTabs.addTab(allTabs.newTab().setText("TWO"));
+        allTabs.addTab(allTabs.newTab().setText("THREE"));
+    }
+
+    private void bindWidgetsWithAnEvent()
+    {
+        allTabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                setCurrentTabFragment(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+    }
+
+    private void setCurrentTabFragment(int tabPosition)
+    {
+        switch (tabPosition)
+        {
+            case 0 :
+                replaceFragment(fragmentOne);
+                break;
+            case 1 :
+                replaceFragment(fragmentTwo);
+                break;
+            case 2 :
+                replaceFragment(fragmentThree);
+                break;
+        }
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frame_container, fragment);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.commit();
+    }
+}
